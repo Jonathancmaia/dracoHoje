a9249c12bc096faf98702de7a1bf48b33bf10b24
jonathancmaia <jonathancmaia@gmail.com>
2021-11-15 14:49:59 -0300

    alteração do retorno de dados

--- app/Http/Controllers/DracoController.php
+++ app/Http/Controllers/DracoController.php
@@ -10,18 +10,18 @@ class DracoController extends Controller
     }
 
     function getDracoDaily(){
-        return $this->Draco->whereRaw('`created_at` > (NOW() - INTERVAL 1 DAY)')->get('value', 'created_at');
+        return $this->Draco->whereRaw('`created_at` > (NOW() - INTERVAL 1 DAY)')->get();
     }
 
     function getDracoWeekly(){
-        return $this->Draco->whereRaw('`created_at` > (NOW() - INTERVAL 7 DAY)')->get('value', 'created_at');
+        return $this->Draco->whereRaw('`created_at` > (NOW() - INTERVAL 7 DAY)')->get();
     }
 
     function getDracoMonthly(){
-        return $this->Draco->whereRaw('`created_at` > (NOW() - INTERVAL 30 DAY)')->get('value', 'created_at');
+        return $this->Draco->whereRaw('`created_at` > (NOW() - INTERVAL 30 DAY)')->get();
     }
 
     function getDracoAll(){
-        return $this->Draco->all('value', 'created_at');
+        return $this->Draco->all();
     }
 }
